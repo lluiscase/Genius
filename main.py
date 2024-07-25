fabb876a5df8df948b068fe6854c0ed5881ffe36
lluiscase <{154901202}+lluiscase@users.noreply.github.com>
2024-07-24 21:30:55 -0300

    Adicionando verificação de escolha

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ O que deve ter no codigo:
 - Adicionar uma musica 8-bit(futuramente)?# Genius
 -Receber a resposta do usuario
 - verificação da resposta
+- Loop
 # Genius'''
 
 import random;
 
 colors = ["vermelho","azul","amarelo","verde"]
-y=0
 reply = []
 #O "Randomizador" de cores
 def bot(i=2):
     selection_for_bot = []
-    x=0
-    while i > x:
+    while i > 0:
         choose_bot = random.randint(0,3)
         bot = colors[choose_bot]
         selection_for_bot.append(bot)
@@ -28,14 +27,24 @@ def bot(i=2):
     return selection_for_bot
         
 
-#Receber a resposta do usuario
 choose = bot()  
 print(choose)
-while len(choose) > y:
-    player = input(str(print("Quais foram as cores dadas pelo robo?")))
-    reply.append(player)
-    y +=1
 
-print(reply)
+def verified_reply():
+#Receber a resposta do usuario
+    global win 
+    global defeat 
+    for _ in range(len(choose)):
+        player = input(str("Quais foram as cores dadas pelo robo?"))
+        reply.append(player)
 #Verificar a resposta
+    if reply == choose:
+        win += 1
+        return "Acertou"
+    defeat +=1 
+    return "Errou" 
+resultado = verified_reply()
+print(resultado)
 #Marcador de Vitoria e derrota
+
+
