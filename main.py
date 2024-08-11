0f5b40a1555f4b66e4dff3a94a875722b4fdfdc7
lluiscase <{154901202}+lluiscase@users.noreply.github.com>
2024-08-11 12:08:49 -0300

    Marcador de pontos e dificuldade

--- main.py
+++ main.py
@@ -11,40 +11,47 @@ O que deve ter no codigo:
 - Loop
 # Genius'''
 
-import random;
+import random
+difficulty = 1
+defeat = -1
+win = 0
 
 colors = ["vermelho","azul","amarelo","verde"]
 reply = []
+
+while defeat < 0:
 #O "Randomizador" de cores
-def bot(i=2):
-    selection_for_bot = []
-    while i > 0:
-        choose_bot = random.randint(0,3)
-        bot = colors[choose_bot]
-        selection_for_bot.append(bot)
-        print(bot)
-        i-=1
-    return selection_for_bot
+    def bot(i=difficulty):
+        selection_for_bot = []
+        while i > 0:
+            choose_bot = random.randint(0,3)
+            bot = colors[choose_bot]
+            selection_for_bot.append(bot)
+            i-=1
+        return selection_for_bot
+
+    choose = bot()
+    for _ in range(len(choose)):
+        print("Piscou: {}".format(choose[_]))
         
 
-choose = bot()  
-print(choose)
+    def verified_reply():
+        global difficulty
+        global defeat
+        global win
+    #Receber a resposta do usuario
+        for _ in range(len(choose)):
+            player = input(str("Quais foram as cores dadas pelo robo?"))
+            reply.append(player)
+    #Verificar a resposta
+        reply == choose and (difficulty := difficulty + 1, win := win + 1) or (defeat := defeat + 1)
+        return "Acertou" if reply == choose else "Perdeu"
+    resultado = verified_reply()
+    print("Acertos atual: {}".format(win))
+    print("Dificuldade atual: {}".format(difficulty))
+    print(resultado)
+    reply.clear()
+    
 
-def verified_reply():
-#Receber a resposta do usuario
-    global win 
-    global defeat 
-    for _ in range(len(choose)):
-        player = input(str("Quais foram as cores dadas pelo robo?"))
-        reply.append(player)
-#Verificar a resposta
-    if reply == choose:
-        win += 1
-        return "Acertou"
-    defeat +=1 
-    return "Errou" 
-resultado = verified_reply()
-print(resultado)
-#Marcador de Vitoria e derrota
 
 
